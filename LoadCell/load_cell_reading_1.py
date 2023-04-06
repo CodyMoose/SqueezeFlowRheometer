ea2e61d7485f7dce46e3b29c2b50d20b510066d1
CodyMoose <rcmoose2000@gmail.com>
2023-04-05 17:40:43 -0700

    Added load cell calibration values and parsing load cell serial outpus

--- LoadCell/load_cell_reading_1.py
+++ LoadCell/load_cell_reading_1.py
@@ -4,7 +4,18 @@ ports = list(port_list.comports())
 
 for p in ports:
     print(p)
+tareReading = 185570
+calibrationFactor = -12047.76734
 
-ser = serial.Serial("COM5",9600)
+ser = serial.Serial("COM5",115200)
+count = 0
 while True:
-    print(ser.readline())
+    line = ser.readline()
+    line = line.decode("utf-8")[:-2] # strips it down to just the line content
+    # print(line)
+    count += 1
+    if count >= 8:
+        reading = int(line[:-1]) # trim the comma at the end
+        grams = float(reading - tareReading) / float(calibrationFactor)
+        # print(reading)
+        print(grams)

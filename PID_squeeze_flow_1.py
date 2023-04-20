07d8324c7c7050bfe1b5a726be6ac08be8f7699d
CodyMoose <rcmoose2000@gmail.com>
2023-04-20 16:38:28 -0700

    Let derivative error print out larger numbers

--- PID_squeeze_flow_1.py
+++ PID_squeeze_flow_1.py
@@ -263,7 +263,7 @@ def actuator_thread():
         v_new = vel_P + vel_D + vel_I
         actuator.set_vel_mms(v_new)
 
-        out_str = "{:6.2f}{:}, err = {:6.2f}, errI = {:6.2f}, errD = {:6.2f}, pos = {:6.2f}, v = {:6.2f} : vP = {:6.2f}, vI = {:6.2f}, vD = {:6.2f}".format(
+        out_str = "{:6.2f}{:}, err = {:6.2f}, errI = {:6.2f}, errD = {:7.2f}, pos = {:6.2f}, v = {:6.2f} : vP = {:6.2f}, vI = {:6.2f}, vD = {:6.2f}".format(
             force,
             scale.units,
             error,

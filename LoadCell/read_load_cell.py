44004941676263ef482f1deb3fd511ffd9f01125
CodyMoose <rcmoose2000@gmail.com>
2023-05-15 13:21:51 -0700

    Implement live plotting for load cell reading

--- LoadCell/read_load_cell.py
+++ LoadCell/read_load_cell.py
@@ -10,22 +10,46 @@ scale = openscale.OpenScale()
 
 readings = []
 times = []
-max_time_window = 30  # how many seconds of readings to store and plot.
+max_time_window = 7  # how many seconds of readings to store and plot.
 start_time = time()
 
 # style.use("fivethirtyeight")
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+ax1 = fig.add_subplot(1, 1, 1)
+ax2 = ax1.twinx()
+
+color1 = "C0"
+color2 = "C1"
 
 
 def animate(i):
-    global ax
-    ax.clear()
-    ax.plot(times, readings)
+    global ax1, ax2
+    ax1.clear()
+    ax2.clear()
+
+    # Store data & timestamps locally to prevent race conditions due to multithreading
+    timesTemp = times[:]
+    readingsTemp = readings[:]
+    if len(timesTemp) < 1:
+        return
+
+    ax1.set_xlabel("Time [s]")
+    ax1.set_ylabel("data1 [-]", color=color1)
+    ax1.plot(timesTemp, readingsTemp, color1, label="data1")
+    plt.xlim(min(timesTemp), max(max(timesTemp), max_time_window))
+    # ax2.set_ylabel("data2 [-]", color=color2)
+    # ax2.plot(timesTemp, [-2 * r for r in readingsTemp], color2, label="data2")
+    # ax1.set_ylim((-11, 11))
+    # ax2.set_ylim((-22, 22))
+    ax1.tick_params(axis="y", colors=color1)
+    ax1.spines["left"].set_color(color1)
+    # ax2.tick_params(axis="y", colors=color2)
+    # ax2.spines["left"].set_alpha(0)
+    # ax2.spines["right"].set_color(color2)
 
 
 def get_data():
-    global readings, times, max_time_window, scale
+    global readings, times, max_time_window  # , scale
     while True:
         weight = scale.wait_for_calibrated_measurement()
         if weight is None:  # if startup garbage not gone yet
@@ -43,10 +67,14 @@ def get_data():
             readings.pop(0)
             times.pop(0)
 
+        # sleep(0.01)
+
 
 gd = threading.Thread(name="get_data", target=get_data)
 
 gd.start()
+print("hello")
 
 ani = animation.FuncAnimation(fig, animate, interval=10)
+print("hello1")
 plt.show()
